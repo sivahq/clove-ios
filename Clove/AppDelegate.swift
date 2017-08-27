02895a95ec906644e472f36fae7a16d71f3a2a56
Sivaprakash Ragavan <mail@sivragav.com>
2017-08-27 07:17:28 -0400

    Initializing Parse SDK

--- Clove/AppDelegate.swift
+++ Clove/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "APPLICATION_ID"
+            $0.clientKey = "MASTER_KEY"
+            $0.server = "http://localhost:1337/parse"
+        }
+        
+        Parse.initialize(with: configuration)
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = UIColor.white
